129cc5f829a589e56048a6e8dfcd6f42c9b85ff9
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-09-15 23:31:52 +0900

    Use queue instead of *sqs.SQS

--- message.go
+++ message.go
@@ -1,24 +1,24 @@
 package delayd2
 
 import (
-	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/nabeken/aws-go-sqs/queue"
 	"github.com/nabeken/aws-go-sqs/queue/option"
 )
 
 type Sender struct {
-	sqsSvc *sqs.SQS
+	queue *queue.Queue
 }
 
-func (s *Sender) SendMessage(queueName string, duration int, relayTo, payload string) error {
+func NewSender(queue *queue.Queue) *Sender {
+	return &Sender{
+		queue: queue,
+	}
+}
+
+func (s *Sender) SendMessage(duration int, relayTo, payload string) error {
 	attrs := map[string]interface{}{
 		sqsMessageDurationKey: duration,
 		sqsMessageRelayToKey:  relayTo,
 	}
-	q, err := queue.New(s.sqsSvc, queueName)
-	if err != nil {
-		return err
-	}
-
-	return q.SendMessage(payload, option.MessageAttributes(attrs))
+	return s.queue.SendMessage(payload, option.MessageAttributes(attrs))
 }
