b244f2b9134b627b04410a90dd5bc3aa8ce956b3
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-09-17 22:24:11 +0900

    Add SendMessageBatch for bench

--- message.go
+++ message.go
@@ -22,3 +22,41 @@ func (s *Sender) SendMessage(duration int, relayTo, payload string) error {
 	}
 	return s.queue.SendMessage(payload, option.MessageAttributes(attrs))
 }
+
+func (s *Sender) SendMessageBatch(duration int, relayTo string, payloads []string) error {
+	attrs := map[string]interface{}{
+		sqsMessageDurationKey: duration,
+		sqsMessageRelayToKey:  relayTo,
+	}
+
+	remaining := payloads
+	var cur []string
+	for {
+		if len(remaining) == 0 {
+			break
+		}
+
+		if len(remaining) > 10 {
+			cur = remaining[:10]
+		} else {
+			cur = remaining
+		}
+
+		batch := make([]queue.BatchMessage, 0, len(cur))
+
+		for _, body := range cur {
+			batch = append(batch, queue.BatchMessage{
+				Body:    body,
+				Options: []option.SendMessageInput{option.MessageAttributes(attrs)},
+			})
+		}
+
+		// stop immediately if error occurs
+		if err := s.queue.SendMessageBatch(batch...); err != nil {
+			return err
+		}
+		remaining = remaining[len(cur):]
+	}
+
+	return nil
+}
