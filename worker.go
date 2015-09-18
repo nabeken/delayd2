a86611f5e3970c3f71130ae6d4b0c35817fbbc5f
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-09-18 10:00:35 +0900

    worker: Relay concurrently

--- worker.go
+++ worker.go
@@ -25,6 +25,8 @@ type Worker struct {
 	consumer *Consumer
 	relay    *Relay
 
+	relayCh chan releaseJob
+
 	// signaled from external
 	shutdownCh chan struct{}
 
@@ -40,6 +42,7 @@ func NewWorker(workerID string, driver Driver, consumer *Consumer, relay *Relay)
 		consumer: consumer,
 		relay:    relay,
 
+		relayCh:    make(chan releaseJob, runtime.NumCPU()),
 		shutdownCh: make(chan struct{}),
 		stoppedCh:  make(chan struct{}),
 	}
@@ -63,6 +66,9 @@ func (w *Worker) Run() error {
 	for i := 0; i < nCPU; i++ {
 		log.Print("worker: launching consumer process")
 		go w.handleConsume()
+
+		log.Print("worker: launching relay worker process")
+		go w.relayWorker()
 	}
 
 	go w.handleMarkActive()
@@ -135,58 +141,73 @@ func (w *Worker) handleRelease() {
 	for range time.Tick(1 * time.Second) {
 		select {
 		case <-w.shutdownCh:
+			close(w.relayCh)
 			close(w.stoppedCh)
 			return
 		default:
 		}
 
-		begin := time.Now()
-
-		n, err := w.release()
-		if err != nil {
+		if err := w.release(); err != nil {
 			log.Printf("worker: unable to release messages: %s", err)
 			continue
 		}
-
-		end := time.Now()
-
-		if n > 0 {
-			log.Printf("worker: %d messages removed from queue in %s", n, end.Sub(begin))
-		}
 	}
+}
+
+type releaseJob struct {
+	RelayTo  string
+	Messages []releaseMessage
+}
+
+type releaseMessage struct {
+	QueueID string
+	Payload string
 }
 
 // release releases active messages. It returns the number of released message n.
-func (w *Worker) release() (int64, error) {
+func (w *Worker) release() error {
 	messages, err := w.driver.GetActiveMessages()
 
 	if err != nil {
-		return 0, err
+		return err
 	}
 
-	var n int64
+	// we need QueueID to delete from queue
+	batchMap := BuildBatchMap(messages)
 
-	// we need QueueMessage to use QueueID to delete from queue
-	messagesMap := make(map[string][]*QueueMessage)
-	for _, m := range messages {
-		messagesMap[m.RelayTo] = append(messagesMap[m.RelayTo], m)
+	for relayTo, batchMsgs := range batchMap {
+		for _, rms := range batchMsgs {
+			w.relayCh <- releaseJob{
+				RelayTo:  relayTo,
+				Messages: rms,
+			}
+		}
 	}
-
-	for relayTo, ms := range messagesMap {
-		n += w.releaseBatch(relayTo, ms)
-	}
-	return n, nil
+	return nil
 }
 
-func (w *Worker) releaseBatch(relayTo string, messages []*QueueMessage) int64 {
-	payloads := make([]string, 0, len(messages))
-	for _, m := range messages {
+func (w *Worker) relayWorker() {
+	for rj := range w.relayCh {
+		begin := time.Now()
+		n := w.releaseBatch(rj)
+		end := time.Now()
+
+		if n > 0 {
+			log.Printf("worker: %d messages relayed in %s", n, end.Sub(begin))
+		}
+	}
+	log.Print("relayworker: closed")
+}
+
+func (w *Worker) releaseBatch(rj releaseJob) int64 {
+	payloads := make([]string, 0, len(rj.Messages))
+	for _, m := range rj.Messages {
 		payloads = append(payloads, m.Payload)
 	}
 
 	var n int64
 	failedIndex := make(map[int]struct{})
-	if err := w.relay.Relay(relayTo, payloads); err != nil {
+	if err := w.relay.Relay(rj.RelayTo, payloads); err != nil {
 		// only print log here in batch operation
 		// TODO: a dead letter queue support
 		berrs, batchOk := queue.IsBatchError(err)
@@ -203,7 +224,7 @@ func (w *Worker) releaseBatch(relayTo string, messages []*QueueMessage) int64 {
 		}
 	}
 
-	for i, m := range messages {
+	for i, m := range rj.Messages {
 		if _, failed := failedIndex[i]; failed {
 			continue
 		}
@@ -216,4 +237,25 @@ func (w *Worker) releaseBatch(relayTo string, messages []*QueueMessage) int64 {
 	}
 
 	return n
+}
+
+func BuildBatchMap(messages []*QueueMessage) map[string][][]releaseMessage {
+	batchMap := make(map[string][][]releaseMessage)
+	for _, m := range messages {
+		rm := releaseMessage{
+			QueueID: m.QueueID,
+			Payload: m.Payload,
+		}
+
+		var i int
+		i = len(batchMap[m.RelayTo])
+		if i == 0 || len(batchMap[m.RelayTo][i-1]) >= 10 {
+			batchMap[m.RelayTo] = append(batchMap[m.RelayTo], []releaseMessage{})
+		}
+
+		i = len(batchMap[m.RelayTo])
+
+		batchMap[m.RelayTo][i-1] = append(batchMap[m.RelayTo][i-1], rm)
+	}
+	return batchMap
 }
