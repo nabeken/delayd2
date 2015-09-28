fafc5ec5d526aaecea48bd282cfa69097991ceda
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-09-28 20:56:30 +0900

    releng: initial import

---
+++ releng/release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WORKSPACE=$(git rev-parse --show-toplevel)
+
+set -e
+set -x
+
+cd "${WORKSPACE}"/releng
+
+rm -rf bin
+docker pull nabeken/delayd2:latest
+docker run -it --rm --entrypoint sh nabeken/delayd2:latest -c 'tar -C /go -cf - bin | base64' | openssl enc -d -base64 | tar -xvf -
+
+docker build --no-cache -t nabeken/delayd2-release:latest .
+
+docker images nabeken/delayd2-release
