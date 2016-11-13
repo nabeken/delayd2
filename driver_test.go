48fab25648eee813cc9b7d7edc2387845bc5b8c6
TANABE Ken-ichi <nabeken@tknetworks.org>
2016-11-13 15:35:23 +0900

    Fix the test

--- driver_test.go
+++ driver_test.go
@@ -182,7 +182,7 @@ func TestDriver(t *testing.T) {
 				tc.Messages[i].ReleaseAt, messages[i].ReleaseAt,
 			)
 
-			assert.NoError(drv.RemoveMessages([]string{messages[i].QueueID}))
+			assert.NoError(drv.RemoveMessages(messages[i].QueueID))
 		}
 
 		n, err = drv.MarkActive(now.Add(1 * time.Minute))
