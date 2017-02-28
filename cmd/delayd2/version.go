9e80d3244bd82a1c61cc5266f9585ca2c4fc1cf1
TANABE Ken-ichi <nabeken@tknetworks.org>
2017-03-01 00:25:15 +0900

    Just delete cmd

--- cmd/delayd2/version.go
+++
@@ -1,9 +0,0 @@
-package main
-
-const Name string = "delayd2"
-const Version string = "0.2.0.dev"
-
-// GitCommit describes latest commit hash.
-// This value is extracted by git command when building.
-// To set this from outside, use go build -ldflags "-X main.GitCommit \"$(COMMIT)\""
-var GitCommit string
